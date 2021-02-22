525d84a607c31d965cfc61aa2e512cbf245b9543
WiTekh <julien.delbosc@epita.fr>
2021-02-23 00:37:10 +0100

    Prototype Menu Done and Pause Menu in Game1 and Game2 Scenes achieved

---
+++ Assets/Scripts/Menu Scripts/GameMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuManager : MonoBehaviour
+{
+    public GameObject Menu;
+    private bool m_isOpen = false;
+
+    void Update()
+    {
+        //If 'Esc' pressed, pause the InGame time, and display the menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!m_isOpen)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+
+            Menu.SetActive(!m_isOpen);
+            m_isOpen = !m_isOpen;
+        }
+    }
+
+    //Back to menu WITHOUT SAVE
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
