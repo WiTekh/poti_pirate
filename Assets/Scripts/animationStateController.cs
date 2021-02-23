4fd3f0b83528ef06f18540d00cef6bb309897546
WiTekh <julien.delbosc@epita.fr>
2021-02-24 00:27:53 +0100

    Added Character model, Animated it and linked W (resp E) with Walking forward animation (resp Picking Up on the ground)

---
+++ Assets/Scripts/animationStateController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator m_animator;
+
+    int isRunningHash;
+    int pickUpHash;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        isRunningHash = Animator.StringToHash("isRunning");
+        pickUpHash = Animator.StringToHash("pickup");
+    }
+    void Update()
+    {
+        bool l_forwardPressed = Input.GetKey("w");
+        bool l_isEPressed = Input.GetKey("e");
+
+        bool l_isRunning = m_animator.GetBool(isRunningHash);
+
+        if (l_isEPressed)
+        {
+            m_animator.SetTrigger("pickup");
+        }
+        else
+        {
+           m_animator.ResetTrigger("pickup");
+        }
+
+        if (l_forwardPressed && !l_isRunning)
+        {
+            m_animator.SetBool(isRunningHash, true);
+        }
+        if (!l_forwardPressed && l_isRunning)
+        {
+            m_animator.SetBool(isRunningHash, false);
+        }
+    }
+}
