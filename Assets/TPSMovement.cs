5483a0258b6fcea25926825900973113696d745a
WiTekh <julien.delbosc@epita.fr>
2021-02-24 10:33:00 +0100

    Changed Lighting Setting to look better, Added Camera Follow + Animation in all directions, will change running animation due to issue in position reset at the end of frame

--- Assets/TPSMovement.cs
+++ Assets/TPSMovement.cs
@@ -5,11 +5,15 @@ using static Inputs;
 
 public class TPSMovement : MonoBehaviour
 {
-    public CharacterController m_controller;
+    private CharacterController m_controller;
+    public Transform m_camera;
+
     public float m_speed = 6;
+    float turnSmoothVelocity;
 
     void Start()
     {
+        //Getting controller from this GO
         m_controller = GetComponent<CharacterController>();
     }
 
@@ -17,6 +21,7 @@ public class TPSMovement : MonoBehaviour
     {
         Vector3 l_movement = Vector3.zero;
 
+        //Passing Inputs as a Vector3
         if (Input.GetKey(InputArray[0])){
             l_movement.z += 1;
         }
@@ -31,14 +36,16 @@ public class TPSMovement : MonoBehaviour
             l_movement.x += 1;
         }
 
+        //Computing the movement vector
         if (l_movement.magnitude >= 0.1f)
         {
-            float l_targetAngle = Mathf.Atan2(l_movement.x, l_movement.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, l_targetAngle, 0f);
+            float l_targetAngle = Mathf.Atan2(l_movement.x, l_movement.z) * Mathf.Rad2Deg + m_camera.eulerAngles.y; // Compute the angle that faces the camera
+            float l_angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, l_targetAngle, ref turnSmoothVelocity, 0.1f); // Smoothing the angle when we go from moving forwrd to strafing left
 
-            m_controller.Move(l_movement * m_speed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, l_angle, 0f); // applying Rotation
+
+            Vector3 l_moveDirection = Quaternion.Euler(0f, l_targetAngle, 0f) * Vector3.forward;
+            m_controller.Move(l_moveDirection.normalized * m_speed * Time.deltaTime); //Apply movements
         }
-
-        Debug.Log(l_movement); 
     }
 }
