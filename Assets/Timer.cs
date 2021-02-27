32864682bf8de216549bd5bc1d94fb25a5c123da
unknown <alexandre.netchaev@epita.fr>
2021-02-27 03:55:12 +0000

    un max de truc genre l'eau

---
+++ Assets/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float startTime;
+    public Text timer;
+
+    void Start()
+    {
+        timer.text = startTime.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTime <= 0)
+        {
+            Debug.Log("Game Over");
+        }
+        else
+        {
+            startTime -= Time.deltaTime;
+        }
+        timer.text = Mathf.Round(startTime).ToString();
+    }
+}
