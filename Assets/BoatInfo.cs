fed39dfef032540b23cc655a11568c93e83942d0
unknown <alexandre.netchaev@epita.fr>
2021-02-28 23:01:02 +0000

    prout b... mais non je rigole c'est un vrai commit : instanciation + chustom ships + victory zone + back button + colliders islands

---
+++ Assets/BoatInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoatInfo : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EndZone"))
+        {
+            Debug.Log("Victory");
+        }
+    }
+}
---
+++ Assets/Scripts/User Interface/Game2Manager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game2Manager : MonoBehaviour
+{
+    private static GameObject boat;
+    private int boat_choice = Inventory.monbato; 
+    
+    private void Awake()
+    {
+        switch (boat_choice)
+        {
+            case 0 :
+                boat = Instantiate(Resources.Load("military") as GameObject, transform.position, Quaternion.identity);
+                break;
+            case 1 :
+                boat = Instantiate(Resources.Load("boat-fishing") as GameObject, transform.position, Quaternion.identity);
+                break;
+            case 2 :
+                boat = Instantiate(Resources.Load("Boat") as GameObject, transform.position, Quaternion.identity);
+                break;
+        }
+    }
+
+ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
