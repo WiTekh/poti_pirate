b0041140607bc176b1feb367758339fb932fbe78
WiTekh <julien.delbosc@epita.fr>
2021-02-28 20:28:42 +0100

    Setup Game2 Scene with UI linked to inventory, currently doing transition betweenGame1 and Game2

---
+++ Assets/escapeZone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class escapeZone : MonoBehaviour
+{
+    public GameObject confirm;
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject l_collided = other.gameObject;
+        if (l_collided.CompareTag("Player"))
+        {
+            confirm.SetActive(true);
+            
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
