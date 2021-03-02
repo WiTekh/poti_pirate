cef0fe25dbf6251aa17b1aa355c8fea084cf7894
unknown <alexandre.netchaev@epita.fr>
2021-03-02 02:53:36 +0000

    pleins de trucs et de stats

--- Assets/Scripts/boat/BoatInfo.cs
+++ Assets/Scripts/boat/BoatInfo.cs
@@ -3,23 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static Inputs;
 using UnityEngine.UI;
 
 public class BoatInfo : MonoBehaviour
 {
     private Slider helth;
     private int healthstag = 2;
+    private int repairtime = 3;
+    private float timerRepair;
     private float timer;
-    private bool Touched = false;
+    private bool Touched;
+    private bool Repairing;
+    private boatController boat;
 
     private void Start()
     {
+        boat = GetComponent<boatController>();
         timer = 0;
+        timerRepair = 0;
         helth = GameObject.Find("boatHealth").GetComponent<Slider>();
     }
 
     private void Update()
     {
+        // CHEAT CODES //
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Inventory.planks += 5;
+            Inventory.cannonball += 10;
+        }
+
+
         if (Touched)
         {
             timer += Time.deltaTime;
@@ -28,6 +44,27 @@ public class BoatInfo : MonoBehaviour
                 Touched = false;
                 timer = 0;
             }
+        }
+
+        // REPAIR
+        if (Input.GetKeyDown(InputArray[8]) && Inventory.planks > 0 && Repairing == false)
+        {
+            Inventory.planks--;
+            helth.value++;
+            boat.m_speed /= 2;
+            Repairing = true;
+        }
+        
+        if (timerRepair >= repairtime)
+        {
+            Repairing = false;
+            boat.m_speed *= 2;
+            timerRepair = 0;
+        }
+
+        if (Repairing)
+        {
+            timerRepair += Time.deltaTime;
         }
     }
 
