a7b94bb276c2c44d46d14c22c70328294e07df8e
unknown <alexandre.netchaev@epita.fr>
2021-03-02 21:31:59 +0000

    lo

--- Assets/Scripts/boat/BoatInfo.cs
+++ Assets/Scripts/boat/BoatInfo.cs
@@ -8,17 +8,21 @@ using UnityEngine.UI;
 
 public class BoatInfo : MonoBehaviour
 {
+    private GameObject victory;
     private Slider helth;
     private int healthstag = 2;
     private int repairtime = 3;
     private float timerRepair;
     private float timer;
+    private bool Fed;
     private bool Touched;
     private bool Repairing;
     private boatController boat;
 
     private void Start()
     {
+        victory = GameObject.Find("Victory");
+        victory.SetActive(false);
         boat = GetComponent<boatController>();
         timer = 0;
         timerRepair = 0;
@@ -33,6 +37,8 @@ public class BoatInfo : MonoBehaviour
         {
             Inventory.planks += 5;
             Inventory.cannonball += 10;
+            Inventory.food += 10;
+            Inventory.water += 20;
         }
 
 
@@ -46,6 +52,20 @@ public class BoatInfo : MonoBehaviour
             }
         }
 
+        // FOOD
+
+        if (Inventory.food <= 0 && Fed == false)
+        {
+            Fed = true;
+            boat.m_speed /= 4;
+        }
+
+        if (Inventory.food > 0 && Fed)
+        {
+            boat.m_speed *= 4;
+            Fed = false;
+        }
+        
         // REPAIR
         if (Input.GetKeyDown(InputArray[8]) && Inventory.planks > 0 && Repairing == false)
         {
@@ -68,11 +88,16 @@ public class BoatInfo : MonoBehaviour
         }
     }
 
+    // VICTORY
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("EndZone"))
         {
-            Debug.Log("Victory");
+            victory.SetActive(true);
+            Time.timeScale = 0;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
     }
 
